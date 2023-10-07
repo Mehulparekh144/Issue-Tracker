2a7acba144817bc0c5d5c365abd6e9d19351cc89
Mehulparekh144 <mehulparekh144@gmail.com>
2023-10-07 23:29:46 +0530

    Project Initialization

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import { SupabaseAdapter } from '@auth/supabase-adapter'
+import NextAuth from 'next-auth/next'
+import Google from 'next-auth/providers/google'
+
+const handler = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+    })
+  ],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
+  })
+})
