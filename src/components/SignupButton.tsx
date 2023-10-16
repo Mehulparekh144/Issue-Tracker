8a2ff1c366a1fc27704be367409160123dbd371f
Mehulparekh144 <mehulparekh144@gmail.com>
2023-10-16 23:46:45 +0530

    Added one more key status to issue schema

--- src/components/SignupButton.tsx
+++ src/components/SignupButton.tsx
@@ -3,16 +3,13 @@ import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
-  DialogFooter,
   DialogTrigger,
 } from "./ui/dialog";
 import { Button } from "./ui/button";
 import { signIn } from "next-auth/react";
 import { FcGoogle } from "@react-icons/all-files/fc/FcGoogle";
-import { Separator } from "./ui/separator";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
 import AuthTab from "./auth/AuthTab";
+import { Separator } from "./ui/separator";
 
 function SignupButton() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -31,10 +28,10 @@ function SignupButton() {
 
       <DialogContent>
         <AuthTab setIsOpen={setIsOpen} />
-        <Separator />
+        <Separator/>
         <Button
           variant={"secondary"}
-          className="flex items-center justify-center gap-4 mt-4 font-bold"
+          className="shadow-sm flex items-center text-zinc-600 dark:text-zinc-300 justify-center gap-4 mt-4 font-bold"
           onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
         >
           <FcGoogle className="h-6 w-6" /> Sign in with Google
--- src/components/dashboard/IssueTable.tsx
+++ src/components/dashboard/IssueTable.tsx
@@ -18,6 +18,7 @@ interface IssueData {
   assignedDate: string;
   deadlineDate: string;
   priority: string;
+  status: string;
 }
 
 function IssueTable() {
@@ -28,6 +29,12 @@ function IssueTable() {
     low: "bg-primary",
   };
 
+  const statusObject: { [key: string]: string } = {
+    open: "bg-emerald-500",
+    closed: "bg-purple-600",
+  };
+
+  
   const dummyData = [
     {
       issueNo: 1,
@@ -37,6 +44,7 @@ function IssueTable() {
       assignedDate: "2023-10-11",
       deadlineDate: "2023-10-15",
       priority: "urgent",
+      status: "open", // Add the "status" key
     },
     {
       issueNo: 2,
@@ -46,6 +54,7 @@ function IssueTable() {
       assignedDate: "2023-10-10",
       deadlineDate: "2023-10-20",
       priority: "high",
+      status: "closed", // Add the "status" key
     },
     {
       issueNo: 3,
@@ -55,6 +64,7 @@ function IssueTable() {
       assignedDate: "2023-10-12",
       deadlineDate: "2023-10-18",
       priority: "low",
+      status: "open", // Add the "status" key
     },
     {
       issueNo: 4,
@@ -64,6 +74,7 @@ function IssueTable() {
       assignedDate: "2023-10-13",
       deadlineDate: "2023-10-17",
       priority: "medium",
+      status: "open", // Add the "status" key
     },
     {
       issueNo: 5,
@@ -73,46 +84,57 @@ function IssueTable() {
       assignedDate: "2023-10-14",
       deadlineDate: "2023-10-22",
       priority: "high",
+      status: "closed", // Add the "status" key
     },
   ];
 
+  // Now each issue has a "status" key with the value "open."
+
   return (
-
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px] ">Issue No.</TableHead>
-            <TableHead>Issue Title</TableHead>
-            <TableHead>Team Assigned</TableHead>
-            <TableHead>Assigner</TableHead>
-            <TableHead>Assigned Date</TableHead>
-            <TableHead>Deadline Date</TableHead>
-            <TableHead className="text-right">Priority</TableHead>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px] ">Issue No.</TableHead>
+          <TableHead>Issue Title</TableHead>
+          <TableHead>Team Assigned</TableHead>
+          <TableHead>Assigner</TableHead>
+          <TableHead>Assigned Date</TableHead>
+          <TableHead>Deadline Date</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead className="text-right">Priority</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {dummyData.map((item: IssueData) => (
+          <TableRow key={item.issueNo}>
+            <TableCell>{item.issueNo}</TableCell>
+            <TableCell>{item.issueTitle}</TableCell>
+            <TableCell>{item.teamAssigned}</TableCell>
+            <TableCell>{item.assigner}</TableCell>
+            <TableCell>{item.assignedDate}</TableCell>
+            <TableCell>{item.deadlineDate}</TableCell>
+            <TableCell>
+              <Badge
+                className={`${statusObject[item.status]} hover:bg-${
+                  statusObject[item.status]
+                }`}
+              >
+                {item.status}
+              </Badge>
+            </TableCell>
+            <TableCell className="text-right">
+              <Badge
+                className={`${priorityObject[item.priority]} hover:bg-${
+                  priorityObject[item.priority]
+                }`}
+              >
+                {item.priority}
+              </Badge>
+            </TableCell>
           </TableRow>
-        </TableHeader>
-        <TableBody>
-          {dummyData.map((item: IssueData) => (
-            <TableRow key={item.issueNo}>
-              <TableCell>{item.issueNo}</TableCell>
-              <TableCell>{item.issueTitle}</TableCell>
-              <TableCell>{item.teamAssigned}</TableCell>
-              <TableCell>{item.assigner}</TableCell>
-              <TableCell>{item.assignedDate}</TableCell>
-              <TableCell>{item.deadlineDate}</TableCell>
-              <TableCell className="text-right">
-                <Badge
-                  className={`${priorityObject[item.priority]} hover:bg-${
-                    priorityObject[item.priority]
-                  }`}
-                >
-                  {item.priority}
-                </Badge>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-
+        ))}
+      </TableBody>
+    </Table>
   );
 }
 
